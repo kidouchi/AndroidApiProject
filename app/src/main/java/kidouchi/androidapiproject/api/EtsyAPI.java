2e6c4dfe4fdf8e55f39a68961d8d1839afad1d7b
kidouchi <kidouchi@andrew.cmu.edu>
2015-08-29 17:51:26 -0400

    added etsy/google+ api access

---
+++ app/src/main/java/kidouchi/androidapiproject/api/EtsyAPI.java
@@ -0,0 +1,9 @@
+package kidouchi.androidapiproject.api;
+
+/**
+ * Created by iuy407 on 8/29/15.
+ */
+public class EtsyApi {
+
+    private static final String API_KEY = "z7b34vuo0ebwc9ta87txypjo";
+}
